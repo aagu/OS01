b61e2067af21aa99ad7a572fe88d6db0cd2752a4
aagu <aaguisme@gmail.com>
2018-12-24 06:34:51 +0800

    尝试处理鼠键中断

--- kernel/main.c
+++ kernel/main.c
@@ -1,5 +1,6 @@
 void io_hlt(void);
 void io_cli(void);
+void io_sti(void);
 void io_out8(int port, int data);
 int io_load_eflags(void);
 void io_store_eflags(int eflags);
@@ -8,7 +9,8 @@ void init_palette(void);
 void set_palette(int start, int end, unsigned char *rgb);
 void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
 void init_mouse_cursor(char* mouse, char bc);
-void putblock(char* vram, int vxsize, int pxsize, int pysize, int px0, int py0, char* buf, int bxsize);
+void putblock(unsigned char* vram, int vxsize, int pxsize, int pysize, int px0, int py0, char* buf, int bxsize);
+void showFont8(unsigned char *vram, int xsize, int x, int y, char c, char* font);
 
 #define COL8_000000		0
 #define COL8_FF0000		1
@@ -27,16 +29,58 @@ void putblock(char* vram, int vxsize, int pxsize, int pysize, int px0, int py0, 
 #define COL8_008484		14
 #define COL8_848484		15
 
+struct SEGMENT_DESCRIPTOR {
+	short limit_low, base_low;
+	char base_mid, access_right;
+	char limit_high, base_high;
+};
+
+struct GATE_DESCRIPTOR {
+	short offset_low, selector;
+	char dw_count, access_right;
+	short offset_high;
+};
+
+void init_gdtidt(void);
+void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar);
+void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
+void load_gdtr(int limit, int addr);
+void load_idtr(int limit, int addr);
+
+extern char systemFont[16];
+
+void init_pic(void);
+void inthandler21(int *esp);
+void inthandler27(int *esp);
+void inthandler2c(int *esp);
+#define PIC0_ICW1		0x0020
+#define PIC0_OCW2		0x0020
+#define PIC0_IMR		0x0021
+#define PIC0_ICW2		0x0021
+#define PIC0_ICW3		0x0021
+#define PIC0_ICW4		0x0021
+#define PIC1_ICW1		0x00a0
+#define PIC1_OCW2		0x00a0
+#define PIC1_IMR		0x00a1
+#define PIC1_ICW2		0x00a1
+#define PIC1_ICW3		0x00a1
+#define PIC1_ICW4		0x00a1
+
+unsigned char *vram;/* 声明变量vram、用于BYTE [...]地址 */
+
 void main(void)
 {
-	char *vram;/* 声明变量vram、用于BYTE [...]地址 */
 	int xsize, ysize;
 	char mcursor[256];
-
-	init_palette();/* 设定调色板 */
-	vram = (char *) 0xa0000;/* 地址变量赋值 */
+	vram = (unsigned char *) 0xa0000;/* 地址变量赋值 */
 	xsize = 320;
 	ysize = 200;
+
+	init_gdtidt();
+	init_pic();
+	io_sti();
+	init_palette();/* 设定调色板 */
+	
 
 	/* 根据 0xa0000 + x + y * 320 计算坐标 8*/
 	boxfill8(vram, xsize, COL8_848484,  0,         0,          xsize -  1, ysize - 21);
@@ -46,6 +90,9 @@ void main(void)
 
 	init_mouse_cursor(mcursor, COL8_848484);
 	putblock(vram, xsize, 16, 16, 80, 80, mcursor, 16);
+
+	io_out8(PIC0_IMR, 0xf9); /* 开放PIC1和键盘中断(11111001) */
+	io_out8(PIC1_IMR, 0xef); /* 开放鼠标中断(11101111) */
 
 	for (;;) {
 		io_hlt();
@@ -140,10 +187,130 @@ void init_mouse_cursor(char* mouse, char bc) {
       }
 }
 
-void putblock(char* vram, int vxsize, int pxsize, int pysize, int px0, int py0, char* buf, int bxsize) {
+void putblock(unsigned char* vram, int vxsize, int pxsize, int pysize, int px0, int py0, char* buf, int bxsize) {
     int x, y;
     for (y = 0; y < pysize; y++)
       for (x = 0; x < pxsize; x++) {
           vram[(py0+y) * vxsize + (px0+x)] = buf[y * bxsize + x];
       }
+}
+
+void showFont8(unsigned char *vram, int xsize, int x, int y, char c, char* font) {
+    int i;
+    char d;
+
+    for (i = 0; i < 16; i++) {
+        d = font[i]; 
+        if ((d & 0x80) != 0) {vram[(y+i)*xsize + x + 0] = c;}
+        if ((d & 0x40) != 0) {vram[(y+i)*xsize + x + 1] = c;}
+        if ((d & 0x20) != 0) {vram[(y+i)*xsize + x + 2] = c;} 
+        if ((d & 0x10) != 0) {vram[(y+i)*xsize + x + 3] = c;}
+        if ((d & 0x08) != 0) {vram[(y+i)*xsize + x + 4] = c;}
+        if ((d & 0x04) != 0) {vram[(y+i)*xsize + x + 5] = c;}
+        if ((d & 0x02) != 0) {vram[(y+i)*xsize + x + 6] = c;}
+        if ((d & 0x01) != 0) {vram[(y+i)*xsize + x + 7] = c;}
+    }
+
+}
+
+void init_gdtidt(void)
+{
+	struct SEGMENT_DESCRIPTOR *gdt = (struct SEGMENT_DESCRIPTOR *) 0x00270000;
+	struct GATE_DESCRIPTOR    *idt = (struct GATE_DESCRIPTOR    *) 0x0026f800;
+	int i;
+
+	/* GDT初始化 */
+	for (i = 0; i < 8192; i++) {
+		set_segmdesc(gdt + i, 0, 0, 0);
+	}
+	set_segmdesc(gdt + 1, 0xffffffff, 0x00000000, 0x4092);
+	set_segmdesc(gdt + 2, 0x0007ffff, 0x00280000, 0x409a);
+	load_gdtr(0xffff, 0x00270000);
+
+	/* IDT初始化 */
+	for (i = 0; i < 256; i++) {
+		set_gatedesc(idt + i, 0, 0, 0);
+	}
+	load_idtr(0x7ff, 0x0026f800);
+
+	return;
+}
+
+void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar)
+{
+	if (limit > 0xfffff) {
+		ar |= 0x8000; /* G_bit = 1 */
+		limit /= 0x1000;
+	}
+	sd->limit_low    = limit & 0xffff;
+	sd->base_low     = base & 0xffff;
+	sd->base_mid     = (base >> 16) & 0xff;
+	sd->access_right = ar & 0xff;
+	sd->limit_high   = ((limit >> 16) & 0x0f) | ((ar >> 8) & 0xf0);
+	sd->base_high    = (base >> 24) & 0xff;
+	return;
+}
+
+void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar)
+{
+	gd->offset_low   = offset & 0xffff;
+	gd->selector     = selector;
+	gd->dw_count     = (ar >> 8) & 0xff;
+	gd->access_right = ar & 0xff;
+	gd->offset_high  = (offset >> 16) & 0xffff;
+	return;
+}
+
+void init_pic(void)
+/* PIC初始化 */
+{
+	io_out8(PIC0_IMR,  0xff  ); /* 禁止所有中断 */
+	io_out8(PIC1_IMR,  0xff  ); /* 禁止所有中断 */
+
+	io_out8(PIC0_ICW1, 0x11  ); /* 边缘触发模式（edge trigger mode） */
+	io_out8(PIC0_ICW2, 0x20  ); /* IRQ0-7由INT20-27接收 */
+	io_out8(PIC0_ICW3, 1 << 2); /* PIC1由IRQ2相连 */
+	io_out8(PIC0_ICW4, 0x01  ); /* 无缓冲区模式 */
+
+	io_out8(PIC1_ICW1, 0x11  ); /* 边缘触发模式（edge trigger mode） */
+	io_out8(PIC1_ICW2, 0x28  ); /* IRQ8-15由INT28-2f接收 */
+	io_out8(PIC1_ICW3, 2     ); /* PIC1由IRQ2连接 */
+	io_out8(PIC1_ICW4, 0x01  ); /* 无缓冲区模式 */
+
+	io_out8(PIC0_IMR,  0xfb  ); /* 11111011 PIC1以外全部禁止 */
+	io_out8(PIC1_IMR,  0xff  ); /* 11111111 禁止所有中断 */
+
+	return;
+}
+
+void inthandler21(int *esp)
+/* 来自PS/2键盘的中断 */
+{
+	boxfill8(vram, 320, COL8_000000, 0, 0, 32 * 8 - 1, 15);
+	//putfonts8_asc(vram, 320, 0, 0, COL8_FFFFFF, "INT 21 (IRQ-1) : PS/2 keyboard");
+	showFont8(vram, 320, 8, 8, COL8_FFFFFF, systemFont+'K'*16);
+	for (;;) {
+		io_hlt();
+	}
+}
+
+void inthandler2c(int *esp)
+/* 来自PS/2鼠标的中断 */
+{
+	boxfill8(vram, 320, COL8_000000, 0, 0, 32 * 8 - 1, 15);
+	showFont8(vram, 320, 8, 8, COL8_FFFFFF, systemFont+'M'*16);
+	for (;;) {
+		io_hlt();
+	}
+}
+
+void inthandler27(int *esp)
+/* PIC0中断的不完整策略 */
+/* 这个中断在Athlon64X2上通过芯片组提供的便利，只需执行一次 */
+/* 这个中断只是接收，不执行任何操作 */
+/* 为什么不处理？
+	→  因为这个中断可能是电气噪声引发的、只是处理一些重要的情况。*/
+{
+	io_out8(PIC0_OCW2, 0x67); /* 通知PIC的IRQ-07（参考7-1） */
+	return;
 }
