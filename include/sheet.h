3201f2b47b6762b05bb826dc0ea4b2e6e307100f
aagu <aaguisme@gmail.com>
2019-03-17 17:45:34 +0800

    绘制计数器窗口

--- include/sheet.h
+++ include/sheet.h
@@ -14,7 +14,7 @@ typedef struct SHEET {
 } SHEET;
 
 typedef struct SHTCTL {
-	unsigned char *vram;
+	unsigned char *vram, *map;
 	int xsize, ysize, top;
 	struct SHEET *sheets[MAX_SHEETS];
 	struct SHEET sheets0[MAX_SHEETS];
--- kernel/main.c
+++ kernel/main.c
@@ -11,16 +11,18 @@
 
 int kx, ky;
 
+void make_window(unsigned char *buf, int xsize, int ysize, char *title);
+
 void main(void)
 {
     struct BOOTINFO *binfo = (struct BOOTINFO*) 0x0ff0;
     int mx, my, i;
 	char s[40];
     SHTCTL *shtctl;
-	SHEET *sht_back, *sht_mouse;
-	unsigned char *buf_back, mcursor[256];
+	SHEET *sht_back, *sht_mouse, *sht_win;
+	unsigned char *buf_back, mcursor[256], *buf_win;
 
-	unsigned int memtotal;
+	unsigned int memtotal, count = 0;
 	struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
 
 	init_gdt();
@@ -39,21 +41,32 @@ void main(void)
 	shtctl = shtctl_init(memman, binfo->vram, binfo->scrnx, binfo->scrny);
 	sht_back  = sheet_alloc(shtctl);
 	sht_mouse = sheet_alloc(shtctl);
+	sht_win = sheet_alloc(shtctl);
 	buf_back = (unsigned char *) memman_alloc_4k(memman, binfo->scrnx * binfo->scrny);
+	buf_win = (unsigned char *) memman_alloc_4k(memman, 160*52);
 	sheet_setbuf(sht_back, buf_back, binfo->scrnx, binfo->scrny, -1); /* 没有透明色 */
 	sheet_setbuf(sht_mouse, mcursor, 16, 16, 99); /* 透明色号99 */
+	sheet_setbuf(sht_win, buf_win, 160, 52, -1);
 	init_screen8(buf_back, binfo->scrnx, binfo->scrny);
 	init_mouse_cursor(mcursor, 99);
+	make_window(buf_win, 160, 52, "counter");
 	sheet_slide(sht_back, 0, 0);
 	mx = (binfo->scrnx - 16) / 2;
     my = (binfo->scrny - 28 - 16) / 2;  
     sheet_slide(sht_mouse, mx, my);
+	sheet_slide(sht_win, 80, 72);
 	sheet_updown(sht_back,  0);
-	sheet_updown(sht_mouse, 1);
+	sheet_updown(sht_mouse, 2);
+	sheet_updown(sht_win, 1);
 	
 	setscrnbuf(sht_mouse);
 
 	for (;;) {
+		count++; /* 从这里开始 */
+		vsprintf(s, "%010d", count);
+		boxfill8(buf_win, 160, COL8_C6C6C6, 40, 28, 119, 43);
+		showString(buf_win, 160, 40, 28, COL8_000000, s);
+		sheet_refresh(sht_win, 40, 28, 120, 44); /* 到这里结束 */
 		int scode = keyboard_read();
 		if (scode != -1) {
 			if (kx >= binfo->scrnx) {
@@ -68,3 +81,54 @@ void main(void)
 		}
 	}
 }
+
+void make_window(unsigned char *buf, int xsize, int ysize, char *title)
+{
+	static char closebtn[14][16] = {
+		"OOOOOOOOOOOOOOO@",
+		"OQQQQQQQQQQQQQ$@",
+		"OQQQQQQQQQQQQQ$@",
+		"OQQQ@@QQQQ@@QQ$@",
+		"OQQQQ@@QQ@@QQQ$@",
+		"OQQQQQ@@@@QQQQ$@",
+		"OQQQQQQ@@QQQQQ$@",
+		"OQQQQQ@@@@QQQQ$@",
+		"OQQQQ@@QQ@@QQQ$@",
+		"OQQQ@@QQQQ@@QQ$@",
+		"OQQQQQQQQQQQQQ$@",
+		"OQQQQQQQQQQQQQ$@",
+		"O$$$$$$$$$$$$$$@",
+		"@@@@@@@@@@@@@@@@"
+	};
+
+	int x, y;
+	char c;
+	boxfill8(buf, xsize, COL8_C6C6C6, 0, 0, xsize - 1, 0 );
+	boxfill8(buf, xsize, COL8_FFFFFF, 1, 1, xsize - 2, 1 );
+	boxfill8(buf, xsize, COL8_C6C6C6, 0, 0, 0, ysize - 1);
+	boxfill8(buf, xsize, COL8_FFFFFF, 1, 1, 1, ysize - 2);
+	boxfill8(buf, xsize, COL8_848484, xsize - 2, 1, xsize - 2, ysize - 2);
+	boxfill8(buf, xsize, COL8_000000, xsize - 1, 0, xsize - 1, ysize - 1);
+	boxfill8(buf, xsize, COL8_C6C6C6, 2, 2, xsize - 3, ysize - 3);
+	boxfill8(buf, xsize, COL8_000084, 3, 3, xsize - 4, 20 );
+	boxfill8(buf, xsize, COL8_848484, 1, ysize - 2, xsize - 2, ysize - 2);
+	boxfill8(buf, xsize, COL8_000000, 0, ysize - 1, xsize - 1, ysize - 1);
+	showString(buf, xsize, 24, 4, COL8_FFFFFF, title);
+
+	for (y = 0; y < 14; y++) {
+		for (x = 0; x < 16; x++) {
+			c = closebtn[y][x];
+			if (c == '@') {
+				c = COL8_000000;
+			} else if (c == '$') {
+				c = COL8_848484;
+			} else if (c == 'Q') {
+				c = COL8_C6C6C6;
+			} else {
+				c = COL8_FFFFFF;
+			}
+			buf[(5 + y) * xsize + (xsize - 21 + x)] = c;
+		}
+	}
+	return;
+} 
