66d5c741d6436d253c747adea21ed4b15ec8daaf
aagu <aaguisme@gmail.com>
2026-09-01 00:01:08 +0800

    libc: provide freestanding stdarg header

---
+++ libc/include/stdarg.h
@@ -0,0 +1,11 @@
+#ifndef _STDARG_H
+#define _STDARG_H
+
+typedef __builtin_va_list va_list;
+
+#define va_start(ap, last) __builtin_va_start(ap, last)
+#define va_end(ap)         __builtin_va_end(ap)
+#define va_arg(ap, type)   __builtin_va_arg(ap, type)
+#define va_copy(dst, src)  __builtin_va_copy(dst, src)
+
+#endif
